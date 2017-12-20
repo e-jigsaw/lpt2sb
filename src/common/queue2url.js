df1a18af15a67810c8f488fa8bf99aa1edd96281
jigsaw <m@jgs.me>
2017-12-20 16:33:41 +0900

    Add published date

--- src/common/queue2url.js
+++ src/common/queue2url.js
@@ -1,4 +1,9 @@
 const {project_name} = require('../../.config.json')
 
-module.exports = queue =>
-  `https://scrapbox.io/${project_name}/tumblr-${queue.post.id}?body=${queue.body}`
+const zpad = n => n.toString().length === 1 ? `0${n}` : n.toString()
+
+module.exports = queue => {
+  const now = new Date()
+  const date = `${now.getFullYear()}${zpad(now.getMonth() + 1)}${zpad(now.getDate())}\n`
+  return `https://scrapbox.io/${project_name}/tumblr-${queue.post.id}?body=${queue.body}%0A%23${date}%0A`
+}
